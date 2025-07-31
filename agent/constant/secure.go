c98d37c327d0c5354574fb365f5eca3d20316e21
Junkrat77 <2367392933@qq.com>
2025-07-31 08:34:18 +0000

    PullRequest: 538 chore: increase the duration of auth expired

--- agent/constant/secure.go
+++ agent/constant/secure.go
@@ -26,6 +26,6 @@ const (
 	AGENT_PRIVATE_KEY             = "private_key"
 	AGENT_PUBLIC_KEY              = "public_key"
 	AGENT_AUTH_EXPIRED_DURATION   = "auth_expired_duration"
-	DEFAULT_AUTH_EXPIRED_DURATION = 1 * time.Second
+	DEFAULT_AUTH_EXPIRED_DURATION = 10 * time.Second
 	GET_PASSWORD_RPC_TIMEOUT      = 1 * time.Second
 )
